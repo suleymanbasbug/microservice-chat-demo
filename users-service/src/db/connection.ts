47dbae88074b85ff060f92e1777bc2491b93df31
Süleyman başbuğ <suleymanbasbug@hotmail.com>
2021-09-25 20:58:36 +0300

    Setting Up the Authentication Endpoint

--- users-service/src/db/connection.ts
+++ users-service/src/db/connection.ts
@@ -1,16 +1,17 @@
 import config from "config";
-import { Connection, createConnection} from "typeorm";
+import { Connection, createConnection } from "typeorm";
 
-import User from './entities/User';
+import User from "./entities/User";
+import UserSession from "./entities/UserSession";
 
 let connection: Connection;
 
 export const initConnection = async () => {
-    connection = await createConnection({
-        entities: [User],
-        type: "mysql",
-        url: <string>config.get("USERS_SERVICE_DB_URL")
-    });
+  connection = await createConnection({
+    entities: [User, UserSession],
+    type: "mysql",
+    url: <string>config.get("USERS_SERVICE_DB_URL"),
+  });
 };
 
 const getConnection = () => connection;
---
+++ users-service/src/db/migrations/1632589604173-userSessions.ts
@@ -0,0 +1,50 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from "typeorm";
+
+export class UserSessions1632589604173 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.createTable(
+      new Table({
+        columns: [
+          {
+            isPrimary: true,
+            length: "36",
+            name: "id",
+            type: "char",
+          },
+          {
+            length: "36",
+            name: "userId",
+            type: "char",
+          },
+          {
+            default: "now()",
+            name: "createdAt",
+            type: "timestamp",
+          },
+          {
+            name: "expiresAt",
+            type: "datetime",
+          },
+        ],
+        name: "userSessions",
+      })
+    );
+    await queryRunner.createForeignKey(
+      "userSessions",
+      new TableForeignKey({
+        columnNames: ["userId"],
+        referencedColumnNames: ["id"],
+        referencedTableName: "users"
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropTable("userSessions");
+  }
+}
